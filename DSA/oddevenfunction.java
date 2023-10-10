3b20a8caa43ce60a6036534df8e728690b764a9c
awinash <awinashnarute4228@gmail.com>
2023-10-10 20:29:04 +0530

    dsa

---
+++ DSA/oddevenfunction.java
@@ -0,0 +1,28 @@
+package DSA;
+
+import java.util.Scanner;
+
+public class oddevenfunction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        oddeven(n);
+        sc.close();
+        
+    }
+    static void oddeven(int n)
+    {
+        if(n==1)
+        {
+            System.out.println("odd");
+        }
+
+        if(n%2==0)
+        {
+            System.out.println("even");
+        }
+         else{
+            System.out.println("Odd");
+         }
+    }
+}
---
+++ DSA/primebetn2.java
@@ -0,0 +1,48 @@
+package DSA;
+
+import java.util.Scanner;
+
+public class primebetn2 {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        int n = sc.nextInt();
+        int n1 = sc.nextInt();
+
+        prime(n, n1);
+        
+    }
+    static void prime(int n, int n1)
+    {
+        for(int i = n+1; i<n1; i++)
+        {
+            int a = 0;
+               int b = (int)(Math.sqrt(i));
+               for(int j=2;j<=b; j++)
+               {
+                if(i%j==0)
+                {
+                    a=1;
+                    break;
+                    
+                }
+               }
+               if(a==0)
+               {
+                System.out.println(i);
+               }
+        }
+
+    
+
+
+
+
+
+
+
+
+
+        
+    }
+    
+}
