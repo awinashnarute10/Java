13a34cab8f4f435449740b9a5a0b0a7d8a7236a0
awinash <awinashnarute4228@gmail.com>
2024-06-03 12:14:15 +0530

    dsa

--- DSA/Searching/smallestletterthantarget.java
+++ DSA/Searching/smallestletterthantarget.java
@@ -3,43 +3,36 @@ package DSA.Searching;
 public class smallestletterthantarget {
     public static void main(String[] args) {
         char[] array = {'x','x','y','y'};
-        char target = 'z';
+        char target = 'x';
         System.out.println(nextGreatestLetter(array, target));
     }
 
      public static char nextGreatestLetter(char[] letters, char target) {
         
-      for(int i=0; i<letters.length; i++)
-      {
-        if(letters[i]>target)
-        {
-            return letters[i];
-        }
-      }
-      return letters[0];
+      // for(int i=0; i<letters.length; i++)
+      // {
+      //   if(letters[i]>target)
+      //   {
+      //       return letters[i];
+      //   }
+      // }
+      // return letters[0];
 
-    //   binary search 
-    //   char res=letters[0];
-    //     int start=0;
-    //     int end=letters.length-1;
-    //     while(start<=end)
-    //     {
-    //         int mid=start+(end-start)/2;
-    //         if(letters[mid]==target)
-    //         {
-    //             start=mid+1;
-    //         }
-    //         else if(target>letters[mid])
-    //         {
-    //             start=mid+1;
-    //         }
-    //         else if(letters[mid]>target)
-    //         {
-    //             res=letters[mid];
-    //             end=mid-1;
-    //         }
-    //     }
-    //     return res;
+        int s =0, e = letters.length-1;
+        int mid = 0;
+        while (s<=e) {
+           mid = s+(e-s)/2;
+          if(target<letters[mid])
+          {
+                e = mid-1;
+          }
+          else
+          {
+            s = mid+1;
+          }       
+        }
+        System.out.println(s);
+        return s==letters.length? letters[0]:letters[s];
     }
     
 }
